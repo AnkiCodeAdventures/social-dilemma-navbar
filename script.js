74ec734d6d2687c0188f1cb2a800c8063f13ba25
Ankita <ankitaag94@gmail.com>
2024-03-22 21:33:01 +0530

    create notification menu

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ hamburgerIcon.addEventListener("click", handleClick);
 function handleClick() {
   hamburgerMenu.classList.toggle("hide");
 }
+
+const bellIcon = document.querySelector(".bell-container");
+const notificationMenu = document.querySelector("aside");
+
+bellIcon.addEventListener("click", handleBellClick);
+
+function handleBellClick() {
+  notificationMenu.classList.toggle("hide");
+}
